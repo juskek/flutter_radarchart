1a11295c8a0c0e485596e48a0e66599b33120404
Justin Kek <justinkekjq@gmail.com>
2021-08-13 11:48:42 +0100

    nodes and branches working

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
 
 void main() {
   runApp(MyApp());
@@ -9,105 +12,228 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Radar Chart',
+      home: MyHomePage(title: 'Radar Chart Demo'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
-  @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  int nodes = 5;
+  int segments = 4;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  late final AnimationController _controller = AnimationController(
+    duration: Duration(seconds: 3),
+    vsync: this,
+  )..forward();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        // child: CustomPaint(
+        //   painter: RadarChartPainter(_controller.value),
+        // ),
+        child: RadarChartTransition(_controller, nodes, segments),
+      ),
+    );
+  }
+}
+
+class RadarChartTransition extends AnimatedWidget {
+  final AnimationController controller;
+  final int nodes;
+  final int segments;
+
+  // CONSTRUCTOR
+  RadarChartTransition(
+    this.controller,
+    this.nodes,
+    this.segments,
+  ) : super(listenable: controller);
+
+  // GETTER
+  AnimationController get _animationController =>
+      listenable as AnimationController;
+
+  // TWEEN
+  // tween sequence for popping effect
+  TweenSequence<double> tweenSequence = TweenSequence<double>(
+    <TweenSequenceItem<double>>[
+      TweenSequenceItem<double>(
+        tween: Tween<double>(begin: 0, end: 1.1)
+            .chain(CurveTween(curve: Curves.easeIn)),
+        weight: 50,
+      ),
+      TweenSequenceItem<double>(
+        tween: Tween<double>(begin: 1.1, end: 1)
+            .chain(CurveTween(curve: Curves.bounceOut)),
+        weight: 50,
+      ),
+    ],
+  );
+
+  // function which returns the tween sequence as a part of the animation progress
+  Animation<double> _tweenValue(start, end) {
+    return tweenSequence.animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Interval(
+          start,
+          end,
+          curve: Curves.ease,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  // BUILD
+  @override
+  Widget build(BuildContext context) {
+    // TODO: staggered animations with list of tweenSequences
+    // Animation<double> tweenValueStaggered = tweenSequence.animate(
+    //   CurvedAnimation(
+    //     parent: _animationController,
+    //     curve: Interval(
+    //       0.33,
+    //       0.66,
+    //       curve: Curves.ease,
+    //     ),
+    //   ),
+    // );
+    // final tweenValueStaggered = _tweenValue(0.33, 0.66);
+    // TODO: split progress (0-1) into phases
+    // half the time for branches, half the time for nodes
+    double phaseProgress = 1 / nodes;
+    double branchPhaseProgress = 0.5 / nodes;
+    double nodePhaseProgress = 0.5 / nodes;
+    List<double> tweenValue = [];
+    for (var i = 0; i < nodes; i++) {
+      print(i);
+      double startProgress = phaseProgress * i;
+      double endProgress = phaseProgress * (i + 1);
+      double tweenValueTemp = _tweenValue(startProgress, endProgress).value;
+      tweenValue.add(tweenValueTemp);
+    }
+
+    // double tweenValue = tweenSequence.evaluate(_animationController);
+    // double tweenValue1 = _tweenValue(0.33, 0.66).value;
+    // double tweenValue2 = _tweenValue(0.67, 0.99).value;
+
+    // tweenValue.add(tweenValue1);
+    // tweenValue.add(tweenValue2);
+
+    return CustomPaint(
+      painter: RadarChartPainter(tweenValue, nodes, segments),
+    );
+  }
+}
+
+class RadarChartPainter extends CustomPainter {
+  final List<double> animationProgress;
+  // final double animationProgress;
+  final int nodes;
+  final int segments;
+
+  RadarChartPainter(
+    this.animationProgress,
+    this.nodes,
+    this.segments,
+  );
+
+  @override
+  void paint(ui.Canvas canvas, ui.Size size) {
+    // List<TweenSequence<double>> tweenSequenceList = [];
+    // for (int i = 0; i < nodes; i++) {
+    //   tweenSequenceList.add(
+    //     TweenSequence<double>(
+    //       <TweenSequenceItem<double>>[
+    //         TweenSequenceItem<double>(
+    //           tween: Tween<double>(begin: 0, end: 1.1)
+    //               .chain(CurveTween(curve: Curves.easeIn)),
+    //           weight: 50,
+    //         ),
+    //         TweenSequenceItem<double>(
+    //           tween: Tween<double>(begin: 1.1, end: 1)
+    //               .chain(CurveTween(curve: Curves.bounceOut)),
+    //           weight: 50,
+    //         ),
+    //       ],
+    //     ),
+    //   );
+    // }
+    final Offset centerPoint = Offset(0, 0);
+    // print(animationProgress);
+    // final double lineLength = 100 * animationProgress[0];
+    // // paint styles
+    // final lineStyle = Paint()
+    //   ..strokeWidth = 3 * animationProgress[0]
+    //   ..color = Colors.black;
+
+    // math for each branch
+    final angle = 360 / nodes;
+    // list of branch end points (x,y)
+    List<List<double>> points = [];
+    // for each branch
+    for (int i = 0; i < nodes; i++) {
+      final double lineLength = 100 * animationProgress[i];
+      // paint styles
+      final lineStyle = Paint()
+        ..strokeWidth = 3 * animationProgress[i]
+        ..color = Colors.black;
+      final nodeRadius = 5 * animationProgress[i];
+      final nodeStyle = Paint()..color = Colors.black;
+
+      if (i == 0) {
+        // start at 12 o clock
+        points.add([0, -lineLength]);
+      } else {
+        // everything else
+        final currentAngle = i * angle * pi / 180;
+        final x = lineLength * sin(currentAngle); // x
+        final y = -lineLength * cos(currentAngle); // y
+        points.add([x, y]);
+      }
+      // draw branch
+      Offset endPoint = Offset(points[i][0], points[i][1]);
+      canvas.drawLine(centerPoint, endPoint, lineStyle);
+      // draw node
+      canvas.drawCircle(endPoint, nodeRadius, nodeStyle);
+    }
+
+    // points
+
+    // final endPoint = Offset(0, lineLength);
+    // var endPoint = Offset(points[0][0], points[0][1]);
+
+    // paint commands
+    // canvas.drawLine(centerPoint, endPoint, lineStyle);
+    // for (int i = 0; i < nodes; i++) {
+    //   Offset endPoint = Offset(points[i][0], points[i][1]);
+    //   canvas.drawLine(centerPoint, endPoint, lineStyle);
+    // }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    // hook called when CustomPainter is rebuilt
+    // when to repaint, set to true if necessary
+    return true;
   }
 }
