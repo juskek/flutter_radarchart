a38a4ad01a3c1a2d0208a59ea129f02d0bc10866
Justin Kek <justinkekjq@gmail.com>
2021-08-16 14:53:11 +0100

    WORKING: v0

--- lib/main.dart
+++ lib/main.dart
@@ -28,9 +28,11 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   int nodes = 5;
   int segments = 4;
+  List<double> data = [0.7, 0.6, 0.5, 0.8, 0.75];
+  List<String> labels = ['A', 'B', 'C', 'D', 'E'];
 
   late final AnimationController _controller = AnimationController(
-    duration: Duration(seconds: 3),
+    duration: Duration(seconds: 3), // ANITIME
     vsync: this,
   )..forward();
 
@@ -50,7 +52,7 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
         // child: CustomPaint(
         //   painter: RadarChartPainter(_controller.value),
         // ),
-        child: RadarChartTransition(_controller, nodes, segments),
+        child: RadarChartTransition(_controller, nodes, segments, data, labels),
       ),
     );
   }
@@ -60,12 +62,16 @@ class RadarChartTransition extends AnimatedWidget {
   final AnimationController controller;
   final int nodes;
   final int segments;
+  final List<double> data;
+  final List<String> labels;
 
   // CONSTRUCTOR
   RadarChartTransition(
     this.controller,
     this.nodes,
     this.segments,
+    this.data,
+    this.labels,
   ) : super(listenable: controller);
 
   // GETTER
@@ -74,24 +80,27 @@ class RadarChartTransition extends AnimatedWidget {
 
   // TWEEN
   // tween sequence for popping effect
-  TweenSequence<double> tweenSequence = TweenSequence<double>(
+  TweenSequence<double> poppingTweenSeq = TweenSequence<double>(
     <TweenSequenceItem<double>>[
       TweenSequenceItem<double>(
-        tween: Tween<double>(begin: 0, end: 1.1)
+        tween: Tween<double>(begin: 0, end: 1.5)
             .chain(CurveTween(curve: Curves.easeIn)),
-        weight: 50,
+        weight: 30,
       ),
       TweenSequenceItem<double>(
-        tween: Tween<double>(begin: 1.1, end: 1)
+        tween: Tween<double>(begin: 1.5, end: 1)
             .chain(CurveTween(curve: Curves.bounceOut)),
-        weight: 50,
+        weight: 70,
       ),
     ],
   );
 
+  // tween for fast to slow effect
+  Tween<double> easeOutTween = Tween(begin: 0, end: 1);
+
   // function which returns the tween sequence as a part of the animation progress
-  Animation<double> _tweenValue(start, end) {
-    return tweenSequence.animate(
+  Animation<double> _tweenSeqVal(start, end) {
+    return poppingTweenSeq.animate(
       CurvedAnimation(
         parent: _animationController,
         curve: Interval(
@@ -103,88 +112,100 @@ class RadarChartTransition extends AnimatedWidget {
     );
   }
 
+  // function which returns the tween as part of the animation progress
+  Animation<double> _tweenVal(start, end) {
+    return easeOutTween.animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Interval(
+          start,
+          end,
+          curve: Curves.easeOut,
+        ),
+      ),
+    );
+  }
+
   // BUILD
   @override
   Widget build(BuildContext context) {
-    // TODO: staggered animations with list of tweenSequences
-    // Animation<double> tweenValueStaggered = tweenSequence.animate(
-    //   CurvedAnimation(
-    //     parent: _animationController,
-    //     curve: Interval(
-    //       0.33,
-    //       0.66,
-    //       curve: Curves.ease,
-    //     ),
-    //   ),
-    // );
-    // final tweenValueStaggered = _tweenValue(0.33, 0.66);
-    // TODO: split progress (0-1) into phases
-    // half the time for branches, half the time for nodes
-    double phaseProgress = 1 / nodes;
-    double branchPhaseProgress = 0.5 / nodes;
-    double nodePhaseProgress = 0.5 / nodes;
-    List<double> tweenValue = [];
-    for (var i = 0; i < nodes; i++) {
-      print(i);
-      double startProgress = phaseProgress * i;
-      double endProgress = phaseProgress * (i + 1);
-      double tweenValueTemp = _tweenValue(startProgress, endProgress).value;
-      tweenValue.add(tweenValueTemp);
+    if (data.length != nodes) {
+      throw ('Data length and number of nodes are not equal!');
     }
 
-    // double tweenValue = tweenSequence.evaluate(_animationController);
-    // double tweenValue1 = _tweenValue(0.33, 0.66).value;
-    // double tweenValue2 = _tweenValue(0.67, 0.99).value;
+    // half the time for branches, half the time for nodes and segments
+    int totalSegments = segments * nodes;
 
-    // tweenValue.add(tweenValue1);
-    // tweenValue.add(tweenValue2);
+    double branchPhaseProgress = 0.5 / nodes;
+    double nodePhaseProgress = 0.5 / nodes;
+    double segPhaseProgress = 0.5 / totalSegments;
+    double polyPhaseProgress = 0.2 / totalSegments;
+    double branchStartProg = 0;
+    double nodeStartProg = 0.2;
+    double segStartProg = 0.4;
+    double polyStartProg = 0.8;
+
+    // List of tween values for branches
+    List<double> branchTweenVal = [];
+    for (var i = 0; i < nodes; i++) {
+      double startProgress = branchStartProg + branchPhaseProgress * i;
+      double endProgress = branchStartProg + branchPhaseProgress * (i + 1);
+      double tweenValueTemp = _tweenSeqVal(startProgress, endProgress).value;
+      branchTweenVal.add(tweenValueTemp);
+    }
+    // List of tween values for nodes
+    List<double> nodeTweenVal = [];
+    for (var i = 0; i < nodes; i++) {
+      double startProgress = nodeStartProg + nodePhaseProgress * i;
+      double endProgress = nodeStartProg + nodePhaseProgress * (i + 1);
+      double tweenValueTemp = _tweenSeqVal(startProgress, endProgress).value;
+      nodeTweenVal.add(tweenValueTemp);
+    }
+    // List of tween values for segments
+    // needs to be repeated for each layer of segment, for each branch
+
+    List<double> segTweenVal = [];
+    for (var j = 0; j < totalSegments; j++) {
+      double startProgress = segStartProg + segPhaseProgress * j;
+      double endProgress = segStartProg + segPhaseProgress * (j + 1);
+      double tweenValueTemp = _tweenSeqVal(startProgress, endProgress).value;
+      segTweenVal.add(tweenValueTemp);
+    }
+
+    double polyTweenVal =
+        _tweenVal(polyStartProg, (polyStartProg + polyPhaseProgress)).value;
 
     return CustomPaint(
-      painter: RadarChartPainter(tweenValue, nodes, segments),
+      painter: RadarChartPainter(branchTweenVal, nodeTweenVal, segTweenVal,
+          polyTweenVal, nodes, segments, data, labels),
     );
   }
 }
 
 class RadarChartPainter extends CustomPainter {
-  final List<double> animationProgress;
-  // final double animationProgress;
+  final List<double> branchAniProgress;
+  final List<double> nodeAniProgress;
+  final List<double> segAniProgress;
+  final double polyAniProgress;
   final int nodes;
   final int segments;
+  final List<double> data;
+  final List<String> labels;
 
   RadarChartPainter(
-    this.animationProgress,
+    this.branchAniProgress,
+    this.nodeAniProgress,
+    this.segAniProgress,
+    this.polyAniProgress,
     this.nodes,
     this.segments,
+    this.data,
+    this.labels,
   );
 
   @override
   void paint(ui.Canvas canvas, ui.Size size) {
-    // List<TweenSequence<double>> tweenSequenceList = [];
-    // for (int i = 0; i < nodes; i++) {
-    //   tweenSequenceList.add(
-    //     TweenSequence<double>(
-    //       <TweenSequenceItem<double>>[
-    //         TweenSequenceItem<double>(
-    //           tween: Tween<double>(begin: 0, end: 1.1)
-    //               .chain(CurveTween(curve: Curves.easeIn)),
-    //           weight: 50,
-    //         ),
-    //         TweenSequenceItem<double>(
-    //           tween: Tween<double>(begin: 1.1, end: 1)
-    //               .chain(CurveTween(curve: Curves.bounceOut)),
-    //           weight: 50,
-    //         ),
-    //       ],
-    //     ),
-    //   );
-    // }
     final Offset centerPoint = Offset(0, 0);
-    // print(animationProgress);
-    // final double lineLength = 100 * animationProgress[0];
-    // // paint styles
-    // final lineStyle = Paint()
-    //   ..strokeWidth = 3 * animationProgress[0]
-    //   ..color = Colors.black;
 
     // math for each branch
     final angle = 360 / nodes;
@@ -192,12 +213,15 @@ class RadarChartPainter extends CustomPainter {
     List<List<double>> points = [];
     // for each branch
     for (int i = 0; i < nodes; i++) {
-      final double lineLength = 100 * animationProgress[i];
+      final double lineLength = 200 * branchAniProgress[i];
       // paint styles
+      // branch
       final lineStyle = Paint()
-        ..strokeWidth = 3 * animationProgress[i]
+        ..strokeWidth = 3 * branchAniProgress[i]
         ..color = Colors.black;
-      final nodeRadius = 5 * animationProgress[i];
+
+      // node
+      final nodeRadius = 5 * nodeAniProgress[i];
       final nodeStyle = Paint()..color = Colors.black;
 
       if (i == 0) {
@@ -217,17 +241,70 @@ class RadarChartPainter extends CustomPainter {
       canvas.drawCircle(endPoint, nodeRadius, nodeStyle);
     }
 
-    // points
+    // draw segments
+    int totalSegments = segments * nodes;
+    int i = 0;
+    for (var j = 0; j < totalSegments; j++) {
+      if (j >= 0 && j < nodes) {
+        i++;
+        if (i >= nodes) {
+          i = 0;
+        }
+        continue; // skip nodes at center
+      }
+      // segment
+      final segRadius = 2.5 * segAniProgress[j];
+      // segment coord = offset branch (i) * segment no. (0, 1, 2, 3)
+      final segStyle = Paint()..color = Colors.black;
+      var segNo = (j / nodes).floor() / segments;
+      Offset segPoint = Offset(points[i][0], points[i][1]) * segNo.toDouble();
+      canvas.drawCircle(segPoint, segRadius, segStyle);
+      i++;
+      if (i >= nodes) {
+        i = 0;
+      }
+    }
 
-    // final endPoint = Offset(0, lineLength);
-    // var endPoint = Offset(points[0][0], points[0][1]);
+    // draw poly
+    final polyStyle = Paint()
+      ..strokeWidth = 2
+      ..color = Colors.grey.withAlpha(100);
+    List<Offset> dataOffsets = [];
+    for (var i = 0; i < nodes; i++) {
+      var temp = Offset(points[i][0], points[i][1]) * data[i] * polyAniProgress;
+      dataOffsets.add(temp);
+    }
+    Path polyPath = Path();
+    polyPath.addPolygon(dataOffsets, true);
+    canvas.drawPath(polyPath, polyStyle);
 
-    // paint commands
-    // canvas.drawLine(centerPoint, endPoint, lineStyle);
-    // for (int i = 0; i < nodes; i++) {
-    //   Offset endPoint = Offset(points[i][0], points[i][1]);
-    //   canvas.drawLine(centerPoint, endPoint, lineStyle);
-    // }
+    // draw labels
+    double fontHeight = 12.0;
+    TextStyle style = TextStyle(
+      color: Colors.grey.withAlpha((255 * polyAniProgress).toInt()),
+      fontSize: fontHeight,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.normal,
+      fontFamily: 'Open Sans',
+    );
+
+    for (var i = 0; i < nodes; i++) {
+      final paraBuilder = ui.ParagraphBuilder(ui.ParagraphStyle(
+        fontSize: style.fontSize,
+        fontFamily: style.fontFamily,
+        fontStyle: style.fontStyle,
+        fontWeight: style.fontWeight,
+        textAlign: TextAlign.center,
+      ))
+        ..pushStyle(style.getTextStyle());
+      paraBuilder.addText(labels[i]);
+      final ui.Paragraph labelPara = paraBuilder.build()
+        ..layout(ui.ParagraphConstraints(width: size.width));
+      var temp =
+          Offset(points[i][0] - fontHeight / 4, points[i][1] - fontHeight / 2) *
+              1.1;
+      canvas.drawParagraph(labelPara, temp);
+    }
   }
 
   @override
